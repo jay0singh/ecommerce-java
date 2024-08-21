ff03445ab55030f801752a4bd62ac483abbb3ace
jay0singh <jaysingha261@gmail.com>
2024-08-21 12:16:08 +0530

    Review Repository Updated

--- src/main/java/com/ms/ecommerce/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/ms/ecommerce/repository/ReviewRepositoryImpl.java
@@ -2,10 +2,14 @@ package com.ms.ecommerce.repository;
 
 import com.ms.ecommerce.dto.Review;
 
+import java.util.ArrayList;
+
 public class ReviewRepositoryImpl implements ReviewRepository{
     private ReviewRepositoryImpl() {
     }
     private static ReviewRepositoryImpl reviewRepository;
+
+    private ArrayList<Review> reviewArrayList = new ArrayList<>();
 
     public static ReviewRepositoryImpl getInstance() {
         if (reviewRepository == null) {
@@ -17,26 +21,49 @@ public class ReviewRepositoryImpl implements ReviewRepository{
 
     @Override
     public Review addReview(Review review) {
+        if(reviewArrayList.add(review)){
+            return review;
+        }
         return null;
     }
 
     @Override
     public Review[] getAllReview() {
-        return new Review[0];
+        Review[] array = reviewArrayList.toArray(new Review[reviewArrayList.size()]);
+        return array;
     }
 
     @Override
     public Review getReviewById(int id) {
+        for(Review review : reviewArrayList){
+            if(review.getReviewId() == id){
+                return review;
+            }
+        }
         return null;
     }
 
     @Override
     public String deleteReviewById(int id) {
-        return "";
+        Review review = this.getReviewById(id);
+        if(review != null){
+            if(reviewArrayList.remove(review)){
+                return "Done";
+            }
+            else{
+                return "Failed";
+            }
+        }
+        return "Not Found";
     }
 
     @Override
     public Review updateReviewById(int id, Review review) {
+        for(Review review1 : reviewArrayList){
+            if(review1.getReviewId() == id){
+                review1 = review;
+            }
+        }
         return null;
     }
 }
